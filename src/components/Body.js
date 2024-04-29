f440c9726e410c74dfa27a96dca546b8d00dadb4
Pammi <pammisao1234@gmail.com>
2024-04-29 22:45:52 +0530

    fifth commit

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,54 +1,71 @@
 import RestaurantCard from "./RestrauntCard";
 import Shimmer from "./Shimmer";
-import {useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 
 const Body = () => {
-    const[restroList,setrestroList] = useState([]);
-    const[filteredRestro,setfilteredRestro] =useState([]);
-    const[searchText,setsearchText]= useState("");
+  const [restroList, setrestroList] = useState([]);
+  const [filteredRestro, setfilteredRestro] = useState([]);
+  const [searchText, setsearchText] = useState("");
 
-    useEffect(() => {
-        fetchData();
-      }, []);
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-      const fetchData = async () =>{
-        const data= await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
-        const json= await data.json();
-        setrestroList(json?.data?.cards[4].card.card.gridElements.infoWithStyle.restaurants);
-        setfilteredRestro(json?.data?.cards[4].card.card.gridElements.infoWithStyle.restaurants
-            );
-        console.log(restroList);
-      }
-    
-    return (
-        <div className="body">
-            <div className="filter"> 
-            <div className="search">
-                <input type="text" className="search-box" value={searchText} onChange={(e)=>setsearchText(e.target.value)}/>
-                <button onClick={()=>
-                    {
-                        const filteredRestro= restroList.filter(
-                            (res) => res.info.name.toLowerCase().includes(searchText.toLowerCase())
-                        );
-                        setfilteredRestro(filteredRestro);
-                    }
-                }>Search</button>
-            </div>
-                <button className="filter-btn" onClick={()=>{
-                const filteredList = restroList.filter((res)=>res.info.avgRating > 4
-                );
-                setfilteredRestro(filteredList);
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+    setrestroList(
+      json?.data?.cards[4].card.card.gridElements.infoWithStyle.restaurants
+    );
+    setfilteredRestro(
+      json?.data?.cards[4].card.card.gridElements.infoWithStyle.restaurants
+    );
+    console.log(restroList);
+  };
+
+  return (
+    <div className="body">
+      <div className="filter">
+        <div className="search">
+          <input
+            type="text"
+            className="search-box"
+            value={searchText}
+            onChange={(e) => setsearchText(e.target.value)}
+          />
+          <button
+          className="search-btn"
+            onClick={() => {
+              const filteredRestro = restroList.filter((res) =>
+                res.info.name.toLowerCase().includes(searchText.toLowerCase())
+              );
+              setfilteredRestro(filteredRestro);
             }}
-            >
-                Top Rated Products</button>
-            </div>
-            <div className="res-container">
-                {
-                    filteredRestro.map((restraunt) => <RestaurantCard key={restraunt.info.id} resData={restraunt}/>)
-                }
-            </div>
+          >
+            Search
+          </button>
         </div>
-    )
-}
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredList = restroList.filter(
+              (res) => res.info.avgRating > 4
+            );
+            setfilteredRestro(filteredList);
+          }}
+        >
+          Top Rated Products
+        </button>
+      </div>
+      <div className="res-container">
+        {filteredRestro.map((restraunt) => (
+          <RestaurantCard key={restraunt.info.id} resData={restraunt} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Body;
--- src/components/RestrauntCard.js
+++ src/components/RestrauntCard.js
@@ -1,29 +1,31 @@
 import { CDN_URL } from "../utils/constants";
-const RestaurantCard = (props) =>{
-    const {resData}=props;
-    const {
-        cloudinaryImageId,
-        name,
-        avgRating,
-        cuisines,
-        costForTwo,
-        deliveryTime,
-      } = resData?.info;
-    return(
-        <div className="res-card">
-            <img
-        className="rounded-lg"
-        alt="res-logo"
-        src={CDN_URL + cloudinaryImageId}
-      />
-            <img src={resData.image} />
-            <h3>{name}</h3>
-            <h4>{cuisines.join(", ")}</h4>
-            <h4>{avgRating} stars</h4>
-            <h4>₹{costForTwo / 100} FOR TWO</h4>
-            <h4>{deliveryTime} minutes</h4>
+const RestaurantCard = (props) => {
+  const { resData } = props;
+  const { cloudinaryImageId, name, avgRating, cuisines, costForTwo, locality } =
+    resData?.info;
+
+  return (
+    <div className="res-card">
+      <div className="image-container">
+        <img
+          className="rounded-lg"
+          alt="res-logo"
+          // src={CDN_URL + cloudinaryImageId}
+          src="https://news.mit.edu/sites/default/files/styles/news_article__image_gallery/public/images/202312/MIT_Food-Diabetes-01_0.jpg?itok=Mp8FVJkC"
+        />
+        <img src={resData.image} />
+        <div class="overlay">
+          <div className="text">{costForTwo}</div>
         </div>
-    )
-}
+      </div>
+      <div className="res-card-inner">
+        <div className="res-name">{name}</div>
+        <div className="res-rating">{avgRating} stars</div>
+        <div className="res-desc">{cuisines.join(", ")}</div>
+        <div className="res-desc">{locality}</div>
+      </div>
+    </div>
+  );
+};
 
 export default RestaurantCard;
